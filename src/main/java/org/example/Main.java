e5925953eb49576600b30b84120e336fc36e6499
Alper <alper.makcan@gmail.com>
2024-11-11 19:13:34 +0300

    Updates methods with conditions and updates userID

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,40 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+
+        if (isBarking) {
+            return clock < 8 && clock > 0 || clock >= 18 && clock <= 23;
+        }
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return firstAge >= 13 && firstAge <= 19 || secondAge >= 13 && secondAge <= 19 || thirdAge >= 13 && thirdAge <= 19;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+
+        if (isSummer) {
+            return temp >= 25 && temp <= 45;
+        } else {
+            return temp >= 25 && temp <= 35;
+        }
     }
 
     public static double area(double width, double height) {
-       
+
+        if (!(width < 0 || height < 0)){
+            return width * height;
+        } else {
+            return -1;
+        }
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI ;
     }
 }
